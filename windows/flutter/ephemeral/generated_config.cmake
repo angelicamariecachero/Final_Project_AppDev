ca85c244a4f62a89ce5566adbd5c10a2727ae749
Angelica Marie Cachero <angelicamariecachero@gmail.com>
2024-07-22 17:22:49 +0800

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Acer\\Downloads\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Acer\\Downloads\\3rd Year Summer\\APP DEV\\task_master_app" PROJECT_DIR)
+
+set(FLUTTER_VERSION "0.1.0" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 0 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Acer\\Downloads\\flutter"
+  "PROJECT_DIR=C:\\Users\\Acer\\Downloads\\3rd Year Summer\\APP DEV\\task_master_app"
+  "FLUTTER_ROOT=C:\\Users\\Acer\\Downloads\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Acer\\Downloads\\3rd Year Summer\\APP DEV\\task_master_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Acer\\Downloads\\3rd Year Summer\\APP DEV\\task_master_app"
+  "FLUTTER_TARGET=C:\\Users\\Acer\\Downloads\\3rd Year Summer\\APP DEV\\task_master_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9lZGQ4NTQ2MTE2NDU3YmRmMWM1YmRmYjEzZWNiOTQ2M2QyYmI1ZWQ0Lw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Acer\\Downloads\\3rd Year Summer\\APP DEV\\task_master_app\\.dart_tool\\package_config.json"
+)
