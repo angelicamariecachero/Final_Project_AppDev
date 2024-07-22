dea3677ac5d68974f45a92d6e8f80fffa1929734
Angelica Marie Cachero <angelicamariecachero@gmail.com>
2024-07-22 20:17:11 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,24 @@
-import 'package:animated_splash_screen/animated_splash_screen.dart';
-//import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-//import 'package:flutter/widgets.dart';
-import 'package:lottie/lottie.dart';
-import 'package:task_master_app/screens/task_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:task_master_app/splash_screen.dart';
+import 'models/task_data.dart';
+void main( ) {
+  runApp( MyApp());
 
-class SplashScreen extends StatelessWidget {
-  const SplashScreen ({super.key});
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedSplashScreen(
-      splash: Center(
-            child: Lottie.asset(
-              'assets/Animation-1721638500978.json') 
-            ), 
-            nextScreen: TaskScreen(),
-      duration: 5000,
-      splashIconSize: 5000,
-      backgroundColor: Colors.blue, 
+    return ChangeNotifierProvider(
+      create: (context) => TaskData(),
+      child: MaterialApp(
+        home: SplashScreen(),
+        title: 'Task Master',
+        theme: ThemeData(primarySwatch: Colors.blue),
+      ),
     );
   }
-
 }
